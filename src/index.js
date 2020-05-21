7672374f77de388324a24e1e13cd8fa756728b85
Christopher Xu <chris@Christophers-MacBook-Pro.local>
2020-05-21 10:09:38 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './Homepage.css';
+import {Explorer, sites} from "adventure-component-library"
+import axios from 'axios';
+var userAddress;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: null
+        }
+    }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    componentDidMount() {
+        this.getAccountName();
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    async getAccountName() {
+        // eslint-disable-next-line no-undef
+        this.setState({account: await ethereum.enable()}, () => {
+            console.log(this.state.account);
+            userAddress = this.state.account[0];
+        });
+    }
+
+    sendAPIRequest() {
+        var apiAddress;
+        apiAddress = "http://13.56.163.182:8000/transfer-token";
+        axios.post(apiAddress, {
+            ticker: "HAMBURGER",
+            amount: 10,
+            to: userAddress,
+            hookUrl: "done",
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return <div className = "container">
+            <div className="header">
+                <h1>Hamburger Happiness</h1>
+                <h2>A Nice Juicy Hamburger</h2>
+            </div>
+            <div className="burger-container">
+                <img className="burger"
+                     src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1204&q=80"/>
+            </div>
+            <div className="metamask-container">
+                <button className="metamask" onClick={this.sendAPIRequest}>Get Hamburger Token</button>
+            </div>
+            <div className="explorer">
+                <Explorer site={sites.ginandjuice}/>
+            </div>
+        </div>
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
